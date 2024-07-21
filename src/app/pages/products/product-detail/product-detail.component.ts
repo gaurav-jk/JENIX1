925bb22a3f0b1b511172f27b7046f65ef587e5ac
gaurav-jk <gaurav.18jics130@jietjodhpur.ac.in>
2024-07-21 17:50:46 +0530

    type any message here

--- src/app/pages/products/product-detail/product-detail.component.ts
+++ src/app/pages/products/product-detail/product-detail.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA, Component, OnInit } from '@angular/core';
 import { ProductService } from '../../../services/product.service'; // Adjust as per your project structure
-import { Product } from '../../../models/product'; 
+
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
